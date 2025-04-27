b7873389a6a7e6e494e6c49037c5022d3986d8c1
kibazola <matuzolemmk@al.insper.edu.br>
2025-04-27 13:30:08 -0300

    Terceira função (organizado)

--- funçõesEP2.py
+++ funçõesEP2.py
@@ -9,3 +9,25 @@ def rolar_dados(n):
 
         i+=1
     return resultado
+
+#2
+def guardar_dado(dados_r, dados_g, num):
+    dados_g.append(dados_r[num])
+    del dados_r[num]
+    resposta = [dados_r, dados_g]
+    return resposta
+
+#3
+
+def remover_dado(dados_rolados, dados_no_estoque,dado_para_remover):
+    dados_rolados_novo = dados_rolados.copy()
+    dados_rolados_novo.append(dados_no_estoque[dado_para_remover])
+    dados_no_estoque_novo = dados_no_estoque.copy()
+    dados_no_estoque_novo.pop(dado_para_remover)
+
+    novo = [dados_rolados_novo,dados_no_estoque_novo]
+
+    return nov
+
+
+
