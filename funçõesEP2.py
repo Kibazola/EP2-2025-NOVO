c4f1b80ab611e612d89def3c4eb74d9af8ff9f94
domingos0000 <domingosmam@al.insper.edu.br>
2025-05-05 16:09:13 -0300

    simplificando a função cinco (tinha muita coisa desnecessaria)

--- funçõesEP2.py
+++ funçõesEP2.py
@@ -44,34 +44,11 @@ def calcula_pontos_regra_simples(lista_n):
 
 #5
 def calcula_pontos_soma(lista_n):
-    i = 1
-    seq = 0
-    seq_final = 0
-    ig = 0
-    sem = 0
     soma = 0
     for num in lista_n:
         soma += num
-    while i < len(lista_n):
-        if lista_n[i] - lista_n[i - 1] == 1:
-            seq += 1
-            if seq > seq_final:
-                seq_final = seq
-        elif lista_n[i] == lista_n[i - 1]:
-            ig =+ 1
-            if ig > sem:
-                sem = ig
-        else:
-            seq = 0
-        i += 1
-    if sem >= 5:
-        return 50
-    elif seq_final == 4:
-        return 15
-    elif seq_final == 5:
-        return 30
-    else:
-        return soma
+    return soma
+
     
 #6
 def calcula_pontos_sequencia_baixa(lista_n):
